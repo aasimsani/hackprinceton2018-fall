96beb1f54c1f7dfda59d8bd7fee6fdb6414f1972
Aasim Sani <aasimsani05@gmail.com>
2018-11-11 09:24:19 -0500

    made socket system async

--- master/socketio_examples.py
+++ master/socketio_examples.py
@@ -9,7 +9,7 @@ app.config['CHAT_URL'] = os.environ.get('CHAT_URL')
 app.config['POLLS_VOTE_URL'] = os.environ.get('POLLS_VOTE_URL')
 app.config['WHERE_PIN_URL'] = os.environ.get('WHERE_PIN_URL')
 app.config['GOOGLE_MAPS_KEY'] = os.environ.get('GOOGLE_MAPS_KEY')
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode=True)
 
 from audio import bp as audio_bp
 
